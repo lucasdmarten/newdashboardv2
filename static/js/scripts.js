ab4f6de2b9d5ff9c56f030df1fa093d1c1e58d40
lucasdmarten <37054529+lucasdmarten@users.noreply.github.com>
2021-03-03 11:08:31 -0300

    Delete static directory

--- static/js/scripts.js
+++
@@ -1,7 +0,0 @@
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
-});
-//  navbarMenu.classList.toggle('is-active');
